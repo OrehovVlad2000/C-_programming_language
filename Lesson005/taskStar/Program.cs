a0dfc482cc19e5b7c940796fbe060cbaf8246ef3
orehov_vlad <orehov.vlad21@gmsil.com>
2022-09-07 21:34:45 +0600

    add taskStar

---
+++ Lesson005/taskStar/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача "со звездочкой": Разобраться с алгоритмом сортировки методом пузырька. 
+// Реализовать невозрастающую сторировку.
+// [3, 0, 2, 4, -1] -> [4, 3, 2, 0, -1]
+// [1,2,2,3,2] -> [3, 2, 2, 2, 1]
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int[] BubbleSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        for (int j = 0; j < array.Length - i - 1; j++)
+        {
+            if (array[j] < array[j + 1])
+            {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+int[] myArray = GetArray(10, 0, 100);
+Console.WriteLine(String.Join(", ", myArray));
+Console.WriteLine(String.Join(", ", BubbleSort(myArray)));
