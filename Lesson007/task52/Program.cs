d34cd3fc8093d68e5cf2753ff66d068632e7e238
orehov_vlad <orehov.vlad21@gmsil.com>
2022-09-13 21:40:56 +0600

    add task52

---
+++ Lesson007/task52/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее 
+// арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+double[] MeanOfColumns(int[,] matr)
+{
+    double[] result = new double[matr.GetLength(1)];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            double temp = Convert.ToDouble(matr[i, j]) / 
+            Convert.ToDouble(matr.GetLength(0));
+            result[j] += temp;
+        }
+    }
+    return result;
+}
+
+int[,] FillArray(int m, int n)
+{
+    int[,] matr = new int[m, n];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 11);
+        }
+    }
+    return matr;
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = FillArray(3, 4);
+PrintArray(matrix);
+Console.WriteLine(
+    $"Ср. ариф. каждого столбца: {String.Join("; ", MeanOfColumns(matrix))}");
