0eb0776cfa197b2b53e001e37cb680e0e7bb9b96
orehov_vlad <orehov.vlad21@gmsil.com>
2022-09-22 22:39:52 +0600

    add task66

--- Lesson009/task66/Program.cs
+++ Lesson009/task66/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumOfElements(int m, int n)
+{
+    if (m < n) m += SumOfElements(++m, n);
+    return m;
+}
+
+
+Console.Write("Введите число m: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumOfElements(numberM, numberN));
